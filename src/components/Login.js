630099c9c07a4a7ff2e585cb0471e83acce59956
darkusagi99 <darkusagi99@gmail.com>
2023-05-01 18:49:52 +0200

    Correction d'un attribut React / HTML

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
 			</i>Sign in with google</button>
 		</div>
 		<div className="position-absolute bottom-0 start-50 translate-middle-x">
-			<figure class="text-center">
+			<figure className="text-center">
 				<figcaption className="blockquote-footer">
 					By connecting to this application, you accept that all contest information are saved inside the application database.<br />
 					All available data is directly visible inside the interface.<br />
