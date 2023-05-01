202a29c61f6c33a5379b47951a47339cac2a0d81
darkusagi99 <darkusagi99@gmail.com>
2023-05-01 18:17:52 +0200

    Ajout mention RGPD

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,15 +6,26 @@ import '../App.css';
 const Login = () => {
   return (
     <div className="text-center">
-      <button className="btn btn-default btn-lg active" onClick={() => {
-			  signInWithPopup(auth, provider).then((result) => {
-				// The signed-in user info.
-				var user = result.user;
-			  });
+		<div>
+			<button className="btn btn-default btn-lg active" onClick={() => {
+				  signInWithPopup(auth, provider).then((result) => {
+					// The signed-in user info.
+					var user = result.user;
+				  });
 
-            }}>
-	  <i className="fab fa-google">
-	  </i>Sign in with google</button>
+				}}>
+			<i className="fab fa-google">
+			</i>Sign in with google</button>
+		</div>
+		<div className="position-absolute bottom-0 start-50 translate-middle-x">
+			<figure class="text-center">
+				<figcaption className="blockquote-footer">
+					By connecting to this application, you accept that all contest information are saved inside the application database.<br />
+					All available data is directly visible inside the interface.<br />
+					Data can be directly deleted in the interface or by the contest creator.
+				</figcaption>
+			</figure>
+		</div>
     </div>
   )
 }
