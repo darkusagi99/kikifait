4a6f6075f889afdbedd1f477931e7d2d9b4c924e
darkusagi99 <darkusagi99@gmail.com>
2023-05-07 15:07:32 +0200

    Affichage des participants par liste

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -5,6 +5,8 @@ import { useParams  } from "react-router-dom";
 
 import '../App.css';
 
+import Logout from './Logout';
+
 function withParams(Contest) {
   return props => <Contest {...props} params={useParams()} />;
 }
@@ -21,6 +23,7 @@ class Contest extends React.Component {
 		
 		this.state = {
 		  contestData:[],
+		  contestorList:[],
 		  user:this.props.user,
 		  id:id
 		};
@@ -41,8 +44,26 @@ class Contest extends React.Component {
 		
 		const contestRef = ref(db, 'contest/' + this.state.id);
 		onValue(contestRef, (snapshot) => {
+			var contestorListTmp = [];
+			let { contestorList } = snapshot.val();
 			
-			this.setState({contestData : snapshot.val()});
+			// Liste des participants non-vide
+			if (!!contestorList) {
+				
+				// Boucle sur les clés et remise en forme de l'objet
+				Object.keys(contestorList).forEach((contestorKey) => {
+				
+					var tmpEntry = contestorList[contestorKey];
+					tmpEntry.key = contestorKey;
+					
+					contestorListTmp.push(tmpEntry);
+				});
+			}
+			console.log(contestorListTmp);
+			this.setState({
+				contestData : snapshot.val(),
+				contestorList : contestorListTmp
+				});
 		});
 	}
 	
@@ -69,7 +90,6 @@ class Contest extends React.Component {
 	}
 	
 	/** Add ready state */
-	// TODO
 	addReadyState(){
 		const contestorReadyRef = ref(db, 'contest/' + this.state.id + '/contestorList/' + this.state.user.uid + '/ready');
 		set(contestorReadyRef,
@@ -78,7 +98,6 @@ class Contest extends React.Component {
 	}
 	
 	/** Remove ready state */
-	// TODO
 	removeReadyState(){
 		
 		const contestorReadyRef = ref(db, 'contest/' + this.state.id + '/contestorList/' + this.state.user.uid + '/ready');
@@ -92,52 +111,60 @@ class Contest extends React.Component {
 	render(){
 		return (
 			<div className="px-5 d-flex flex-column justify-content-center gap-3 mt-5"> { /* List element DIV */ }
-				<h5 className="card-title">{this.state.contestData.label}</h5>
-				<h6 className="card-subtitle mb-2 text-muted">{this.state.contestData.creator}</h6>
-				<p className="card-text">Nombre de personnes à sélectionner : {this.state.contestData.drawRange}</p>
-				<p className="card-text">Nombre de participants : </p>
-				<p className="card-text">Liste des participants : </p>
-				<p className="card-text">Liste des personnes choisies : </p>
-				<p className="card-text">Statut : {this.state.contestData.status}</p>
 				
-				<div className="card">
-					<div className="card-body">
-						<h5 className="card-title"></h5>
-						<h6 className="card-subtitle mb-2 text-muted"></h6>
-						<p className="card-text">{this.state.user.displayName}</p>
-						<p className="card-text">{this.state.user.email}</p>
-						<p className="card-text">{this.state.user.uid}</p>
-						<p className="card-text">Liste des personnes choisies : </p>
-						<p className="card-text">Statut : </p>
-					</div>
-					<div className="card-footer">
-						<button type="button" className="btn btn btn-outline-danger" onClick={this.addParticipation}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-							  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-							  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-							</svg>
-						</button>
-						<button type="button" className="btn btn btn-outline-danger" onClick={this.removeParticipation}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-							  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-							  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-							</svg>
-						</button>
-						<button type="button" className="btn btn btn-outline-danger" onClick={this.addReadyState}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-							  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-							  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-							</svg>
-						</button>
-						<button type="button" className="btn btn btn-outline-danger" onClick={this.removeReadyState}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-							  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-							  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-							</svg>
-						</button>
-					</div>
+				<div> { /* Header DIV */ }
+					<h1 className="text-center">{this.state.contestData.label}</h1>
+					<h6 className="mb-2 text-muted">{this.state.contestData.creator}</h6>
+					<h6 className="mb-2 text-muted">{this.state.contestData.status}</h6>
+					<h6 className="mb-2 text-muted">Nombre de personnes à sélectionner : {this.state.contestData.drawRange}</h6>
 				</div>
 				
+				{ /* logoff DIV */ }
+				<Logout user={this.props.user} />
+				
+				{ /* Liste participants */ }
+				
+				<div className="px-5 d-flex flex-column justify-content-center gap-3 mt-5"> { /* List element DIV */ }
+				{
+					this.state.contestorList.map(entry => (
+						<div className="card" key={entry.key}>
+								<div className="card-body">
+									<h5 className="card-title">{entry.email}</h5>
+									<h6 className="card-subtitle mb-2 text-muted">{entry.name}</h6>
+									<p className="card-text">Nombre de personnes à sélectionner : {entry.ready ? "Pret" : "Pas Pret"}</p>
+								</div>
+							<div className="card-footer">
+								<button type="button" className="btn btn btn-outline-danger" onClick={this.addParticipation}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+									  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+									  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+									</svg>
+								</button>
+								<button type="button" className="btn btn btn-outline-danger" onClick={this.removeParticipation}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+									  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+									  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+									</svg>
+								</button>
+								<button type="button" className="btn btn btn-outline-danger" onClick={this.addReadyState}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+									  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+									  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+									</svg>
+								</button>
+								<button type="button" className="btn btn btn-outline-danger" onClick={this.removeReadyState}>
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+									  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+									  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+									</svg>
+								</button>
+							</div>
+						</div>
+							
+						)
+					)
+				}
+				</div> 
 				
 			</div> 
 		)
